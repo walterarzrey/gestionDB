c6ce1a8ffaa955648c09881847ae207ce90db03a
Janessy Ricaldi Astuhuamán <rjanessy@gmail.com>
2018-04-20 20:50:50 -0500

    modificaciones

---
+++ database/migrations/2018_04_20_181603_create_process_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProcessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('state',40);
+            $table->date('date_reception');
+            $table->date('date_ending');
+            $table->unsignedInteger('document_id');
+            $table->foreign('document_id')->references('id')->on('documents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process');
+    }
+}
