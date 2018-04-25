a4f7a44b569a25c78100278690ac40c871b74818
luis cardenas zavallos <luisleocz@gmail.com>
2018-04-25 15:22:50 -0500

    luis cardenas

---
+++ database/migrations/2018_04_25_194937_create_document_type_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',80);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_types');
+    }
+}
