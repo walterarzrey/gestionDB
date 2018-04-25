6055ea5d6e5f2c7699fb54b0c40549210cd189fe
Frank Serrano Basilio <frankserranobasilio@gmail.com>
2018-04-25 16:04:22 -0500

    SERRANO BASILIO FRANK

---
+++ database/migrations/2018_04_24_032211_create_process_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProcessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('state');
+            $table->date('date_reception');
+            $table->date('date_conclusion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process');
+    }
+}
