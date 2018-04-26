14bfee8b1d3103ab77c4f55aeca078676fca0782
Veliz Ricapa Paul <pulpo8464@gmail.com>
2018-04-26 23:27:23 +0200

    Apoyame

---
+++ database/migrations/2018_04_26_205104_create_procceding_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProccedingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('procceding', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',40);
+            $table->unsignedInteger('process_id');
+            $table->foreign('process_id')->references('id')->on('process');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('procceding');
+    }
+}
---
+++ database/migrations/2018_04_26_205206_create_attached_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attached', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',30);
+            $table->unsignedInteger('process_id');
+            $table->foreign('process_id')->references('id')->on('process');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attached');
+    }
+}
