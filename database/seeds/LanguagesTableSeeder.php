2abdc364fe05c577b34623263a7fee684cce07e8
Daniel Paul Inga Perez <joselucholucho@gmail.com>
2018-04-28 01:50:34 -0500

    TAREA TERMINADA

---
+++ database/seeds/LanguagesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $idiomas = [
+            'alemán',
+            'catalán',
+            'francés',
+            'español ',
+            'eslovaco'
+        ];
+        for($i=1;$i<20;$i++){
+            DB::table('languages')->insert([
+                'language' => $idiomas[rand(0,5)],
+                ]);
+        }
+    }
+}
